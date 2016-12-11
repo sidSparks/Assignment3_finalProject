68094edee4d7abfa694d74f30b9acc0a4361006c
Sydney Parker <parkersydney21@gmail.com>
2016-12-11 12:16:58 -0500

    Revert "Final_Project"
    This reverts commit 8abdd6b518c121ef80c9d961df4fb0f45ea30c5d.

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=sparks_cycle',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-];
